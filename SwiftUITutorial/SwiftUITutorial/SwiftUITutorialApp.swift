04fd0cf7e531775f28bb8e36fc96d7ce1bc9397b
Choihyunjin <guswls3373@gmail.com>
2023-04-16 15:11:29 +0900

    chore: init project

---
+++ SwiftUITutorial/SwiftUITutorial/SwiftUITutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITutorialApp.swift
+//  SwiftUITutorial
+//
+//  Created by Tophy on 2023/04/16.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
